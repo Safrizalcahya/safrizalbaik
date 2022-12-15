e772f4d7aa5887bd6fa8ea84ff436c3ce674a572
safrizalcahya <mauluddiva25@gmail.com>
2022-12-15 17:27:06 +0700

    kedua

--- src/Task.js
+++ src/Task.js
@@ -1,4 +1,4 @@
-const url = "YOURDEPLOYURL" // TODO: replace this
+const url = "https://magnificent-speculoos-55f218.netlify.app" // TODO: replace this
 const studentName = 'Safrizal Cahya Nugraha' // TODO: replace this
 const studentId = 'FE3232849' // TODO: replace this
 
