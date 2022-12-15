b0812ba063e2e302a7840d081a29a6bc7475aa36
safrizalcahya <mauluddiva25@gmail.com>
2022-12-15 17:18:06 +0700

    komit pertama

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL" // TODO: replace this
+const studentName = 'Safrizal Cahya Nugraha' // TODO: replace this
+const studentId = 'FE3232849' // TODO: replace this
+
+export { url, studentName, studentId }
